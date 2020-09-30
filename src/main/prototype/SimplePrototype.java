9ded76a94fcbf5dc08cd2e67ef768b78b4d129f1
whatbugyou <1050466416@qq.com>
2020-09-30 11:08:21 +0800

    update the test of prototype,the difference is clear in time costing between `new` and `clone`

--- src/main/prototype/SimplePrototype.java
+++ src/main/prototype/SimplePrototype.java
@@ -1,5 +1,12 @@
 package main.prototype;
 
 public class SimplePrototype extends ProtypeAbs {
-
+    SimplePrototype(){
+        //the time costing progress
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/prototype/SimplePrototypeTest.java
+++ src/main/prototype/SimplePrototypeTest.java
@@ -8,10 +8,18 @@ class SimplePrototypeTest {
 
     @Test
     void testClone() {
+        long times = 100;
+        long t1 = System.currentTimeMillis();
+        for (int i = 0; i < times; i++) {
+            SimplePrototype simplePrototype = new SimplePrototype();
+        }
+        long t2 = System.currentTimeMillis();
         SimplePrototype simplePrototype = new SimplePrototype();
-        SimplePrototype clone = null;
-        clone = simplePrototype.clone();
-        boolean b = simplePrototype == clone;
-        System.out.println(b); //false
+        for (int i = 0; i < times; i++) {
+            SimplePrototype clone = simplePrototype.clone();
+        }
+        long t3 = System.currentTimeMillis();
+        System.out.println("new :"+(t2-t1)+ "ms");
+        System.out.println("clone :"+(t3-t2)+"ms");
     }
 }
