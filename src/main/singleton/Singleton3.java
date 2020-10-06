0c494d2b8b58e85890ea08c341166a205610a140
whatbugyou <1050466416@qq.com>
2020-10-06 11:23:50 +0800

    Singleton3 bug fixed

--- src/main/singleton/Singleton3.java
+++ src/main/singleton/Singleton3.java
@@ -4,7 +4,7 @@ package main.singleton;
  * Singleton with lazy mode by static inner class
  */
 public class Singleton3 {
-    Singleton3(){}
+    private Singleton3(){}
     private static class SingletonHolder{
         private static Singleton3 instance = new Singleton3();
     }
