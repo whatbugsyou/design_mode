63438db2c6c151a47fb17a2e3fde06481fddb658
whatbugsyou <1050466416@qq.com>
2023-02-24 17:13:55 +0800

    update: factory method comment

--- src/main/factoryMethod/AbstractFactory.java
+++ src/main/factoryMethod/AbstractFactory.java
@@ -1,6 +1,7 @@
 package main.factoryMethod;
 
 public abstract class AbstractFactory {
+    //bizCode is the so-called factory method.
     public void bizCode(){
         Product product = createProduct();
         product.work();
